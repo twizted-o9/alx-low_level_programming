b1520db65dc0897e293e54181f23d0bc50191a31
twizted-o9 <tkwawmawuko@gmail.com>
2023-08-29 18:19:23 +0000

    try 1

--- 0x12-singly_linked_lists/0x10-variadic_functions/0-main.c
+++ 0x12-singly_linked_lists/0x10-variadic_functions/0-main.c
@@ -3,8 +3,11 @@
 
 int main(void)
 {
-    unsigned int sum = sum_them_all(4, 10, 20, 30, 40);
-    printf("Sum: %u\n", sum);
+    int result1 = sum_them_all(4, 100, 200, 300, 422);
+    int result2 = sum_them_all(2, 200, 300);
+
+    printf("%d\n%d\n", result1, result2);
+
     return 0;
 }
 
